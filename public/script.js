e93b2f388c56d2257b7614bf3767d08e22c52a3a
Ryan Kent <imsolost@gmail.com>
2017-08-09 11:05:25 -0700

    updating functions to es6

--- public/script.js
+++ public/script.js
@@ -1,11 +1,26 @@
 console.log("Sanity Check: JS is working!");
 
-$(document).ready(function(){
+$(document).ready( () => {
 
-  getAllBooks()
+  // get all the data on load of the page
+  getAllBooks();
 
-  $(document).on('click', '.delete-btn', () => {
-    let id = $(this).data('id')
+  $('#new-book-form').on('submit', function(event) {
+    event.preventDefault()
+    const newBookData = $(this).serialize();
+    console.log('newBookData',newBookData);
+    $(this).trigger("reset");
+    $.ajax({
+      method: 'POST',
+      url: 'http://mutably.herokuapp.com/books/',
+      data: newBookData,
+      success: getAllBooks()
+    })
+  })
+
+  // becasue the delete-btn is added dynamically, the click handler needs to be written like such, bound to the document
+  $(document).on('click', '.delete-btn', function() {
+    const id = $(this).data('id')
     $.ajax({
       method: 'DELETE',
       url: 'http://mutably.herokuapp.com/books/'+id,
@@ -13,6 +28,31 @@ $(document).ready(function(){
     })
   })
 
+  $(document).on('click', '.edit-btn', function() {
+    const id = $(this).data('id')
+
+    // hide the static title, show the input field
+    $('.title-'+id).hide()
+    $('.input-'+id).show()
+
+    // hide the edit button, show the save button
+    $('.edit-'+id).hide()
+    $('.save-'+id).show()
+
+  })
+
+  $(document).on('click', '.save-btn', function() {
+    const id = $(this).data('id')
+
+    // grab the user's inputted data
+    const updatedTitle = $('.input-'+id+' input').val()
+    $.ajax({
+      method: 'PUT',
+      url: 'http://mutably.herokuapp.com/books/'+id,
+      data: {title: updatedTitle},
+      success: handleBookUpdateResponse
+    })
+  })
 });
 
 function getAllBooks() {
@@ -20,8 +60,7 @@ function getAllBooks() {
   $.ajax({
     method: 'GET',
     url: 'http://mutably.herokuapp.com/books'
-  }).done( (data) => {
-    // console.log(data);
+  }).done(function(data) {
     for (let i=0; i<data.books.length; i++) {
       $('.list-group').append('<li class="list-group-item item-'+data.books[i]._id+'">'
       +'<button class="btn btn-primary edit-btn edit-'+data.books[i]._id+'" data-id="'+data.books[i]._id+'">Edit</button>'
@@ -36,8 +75,20 @@ function getAllBooks() {
 
 function handleBookDeleteResponse(data) {
   console.log('handleBookDeleteResponse got ', data);
-  var bookId = data._id;
-  var $row = $('.item-' + bookId);
+  const bookId = data._id;
+  const $row = $('.item-' + bookId);
   // remove that book row
   $row.remove();
+}
+
+function handleBookUpdateResponse(data) {
+  const id = data._id;
+
+  // replace the old title with the new title
+  $('.title-'+id).html('&nbsp;'+data.title)
+
+  $('.title-'+id).show()
+  $('.input-'+id).hide()
+  $('.edit-'+id).show()
+  $('.save-'+id).hide()
 }
