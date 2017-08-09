b9dee569dcf96acfce56353b77d0e1583e1616c1
Ryan Kent <imsolost@gmail.com>
2017-08-09 15:14:43 -0700

    updated display books to consolidate code

--- public/script.js
+++ public/script.js
@@ -29,11 +29,9 @@ $(document).ready( () => {
   $(document).on('click', '.edit-btn', function() {
     const id = $(this).data('id')
 
-    // hide the static title, show the input field
     $('.title-'+id).hide()
     $('.input-'+id).show()
 
-    // hide the edit button, show the save button
     $('.edit-'+id).hide()
     $('.save-'+id).show()
 
@@ -57,22 +55,8 @@ const getAllBooks = () => {
   $('.list-group').html('')
   $.ajax({
     method: 'GET',
-    url: 'http://mutably.herokuapp.com/books'
-  }).done(function(data) {
-    for (let i=0; i<data.books.length; i++) {
-      $('.list-group').append('<li class="list-group-item item-'+data.books[i]._id+'">'
-
-      +'<button class="btn btn-primary edit-btn edit-'+data.books[i]._id+'" data-id="'+data.books[i]._id+'">Edit</button>'
-
-      +'<button class="btn btn-success save-btn save-'+data.books[i]._id+'" data-id="'+data.books[i]._id+'">Save</button>'
-
-      +'<span class="title-'+data.books[i]._id+'">&nbsp;'+data.books[i].title+'</span>'
-
-      +'<span class="form-inline edit-form input-'+data.books[i]._id+'">&nbsp;<input class="form-control" value="'+data.books[i].title+'"/></span>'
-
-      +'<button class="btn btn-danger delete-btn pull-right" data-id="'+data.books[i]._id+'">Delete</button>'
-      +'</li>')
-    }
+    url: 'http://mutably.herokuapp.com/books',
+    success: displayBooks
   })
 }
 
@@ -92,4 +76,17 @@ const handleUpdate = (data) => {
   $('.input-'+id).hide()
   $('.edit-'+id).show()
   $('.save-'+id).hide()
+}
+
+const displayBooks = (data) => {
+  for (let i=0; i<data.books.length; i++) {
+    $('.list-group').append(
+      `<li class="list-group-item item-${data.books[i]._id}">
+        <button class="btn btn-primary edit-btn edit-${data.books[i]._id}" data-id="${data.books[i]._id}">Edit</button>
+        <button class="btn btn-success save-btn save-${data.books[i]._id}" data-id="${data.books[i]._id}">Save</button>
+        <span class="title-${data.books[i]._id}">&nbsp;${data.books[i].title}</span>
+        <span class="form-inline edit-form input-${data.books[i]._id}">&nbsp;<input class="form-control" value="${data.books[i].title}"/></span>
+        <button class="btn btn-danger delete-btn pull-right" data-id="${data.books[i]._id}">Delete</button>
+      </li>`)
+  }
 }
